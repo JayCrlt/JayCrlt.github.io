2f9b01324742ce7899de8c0cef65024b6e966d03
Jérémy T. Carlot <47741658+JayCrlt@users.noreply.github.com>
2022-12-21 19:00:59 +0100

    only starting at 10 pictures left...

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
         $('.less').show();
         shown = $('.gallery-item.item-4x3:visible').length+20;
         if(shown< items) {
-          $('.gallery-item.item-4x3:lt('+shown+')').show(300);
+          $('.gallery-item.item-4x3:lt('+shown+')').show();
         } else {
-          $('.gallery-item.item-4x3:lt('+items+')').show(300);
+          $('.gallery-item.item-4x3:lt('+items+')').show();
           $('.more').hide();
         }
     });
     $('.less').click(function () {
-        $('.gallery-item.item-4x3').not(':lt(10)').hide(300);
+        $('.gallery-item.item-4x3').not(':lt(10)').hide();
         $('.more').show();
         $('.less').hide();
     });

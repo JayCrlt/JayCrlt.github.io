cc882122a1fafe91c1a780c1e2edb7b111fe12de
JayCrlt <jeremy.carlot@hotmail.fr>
2025-10-22 09:08:25 +0200

    fix auto ref cit

--- update_citations.R
+++ update_citations.R
@@ -1,4 +1,6 @@
-library(scholar) ; library(jsonlite)
+packages <- c("jsonlite", "scholar")
+for (pkg in packages) {if (!require(pkg, character.only = TRUE)) {install.packages(pkg, repos = "https://cloud.r-project.org") 
+  library(pkg, character.only = TRUE)}}
 
 pubs <- get_publications("Eotjew0AAAAJ&hl=fr") 
 write_json(pubs[, c("title", "pubid", "cites")], "citations.json", pretty = TRUE)

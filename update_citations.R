27af04d5997378cf7ad31625e6afc6f9a196f5f1
JayCrlt <jeremy.carlot@hotmail.fr>
2025-10-22 09:10:48 +0200

    Update all files

--- update_citations.R
+++ update_citations.R
@@ -1,6 +1,6 @@
 packages <- c("jsonlite", "scholar")
-for (pkg in packages) {if (!require(pkg, character.only = TRUE)) {install.packages(pkg, repos = "https://cloud.r-project.org") 
+for (pkg in packages) {if (!require(pkg, character.only = TRUE)) {install.packages(pkg, repos = "https://cloud.r-project.org", dependencies = TRUE) 
   library(pkg, character.only = TRUE)}}
 
-pubs <- get_publications("Eotjew0AAAAJ&hl=fr") 
+pubs <- get_publications("Eotjew0AAAAJ&hl") 
 write_json(pubs[, c("title", "pubid", "cites")], "citations.json", pretty = TRUE)

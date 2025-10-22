6648203c134c73bdbd43787fb513b8d43328317d
JayCrlt <jeremy.carlot@hotmail.fr>
2025-10-22 08:06:15 +0200

    Update all files

--- update_citations.R
+++ update_citations.R
@@ -1,6 +1,4 @@
-library(scholar)
-library(jsonlite)
-library(tidyverse)
-id <- "Eotjew0AAAAJ&hl=fr"
-pubs <- get_publications(id) |> arrange((-year))
+library(scholar) ; library(jsonlite)
+
+pubs <- get_publications("Eotjew0AAAAJ&hl=fr") 
 write_json(pubs[, c("title", "pubid", "cites")], "citations.json", pretty = TRUE)

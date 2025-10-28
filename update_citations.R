a443c476d4d53ce9a0653f2c32bf02dd2d78508d
JayCrlt <jeremy.carlot@hotmail.fr>
2025-10-28 14:59:49 +0100

    update AltMetric

--- update_citations.R
+++ update_citations.R
@@ -22,15 +22,10 @@ pubs <- data$articles
 # Create compact citation format
 citation <- lapply(pubs, function(x) {
   cites_value <- if (!is.null(x[["cited_by"]][["value"]])) as.integer(x[["cited_by"]][["value"]]) else 0
-  
   pubid_value <- if (!is.null(x[["citation_id"]])) x[["citation_id"]] else ""
-  
-  list(
-    title = x[["title"]],
-    pubid = pubid_value,
-    cites = cites_value
-  )
-})
+  # Remove your Google Scholar ID prefix (everything before and including ':')
+  pubid_clean <- sub("^Eotjew0AAAAJ:", "", pubid_value)
+  list(title = x[["title"]], pubid = pubid_clean, cites = cites_value)})
 
 # Convert to JSON
 json_output <- toJSON(citation, pretty = TRUE, auto_unbox = TRUE)
